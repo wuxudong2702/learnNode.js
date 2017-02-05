48743de4a8ce209a7d5ec192b405bc85190c4990
MagicConch <350043743@qq.com>
2017-02-05 20:20:55 +0800

    day 4

--- firstHttpServer/server.js
+++ firstHttpServer/server.js
@@ -3,15 +3,22 @@ var url = require("url");
 
 function start(route,handle){
 function onRequest(request,response){
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " +　pathname + " received!");
 
-    route(handle,pathname,response);
-}
-    // response.writeHead(200,{ "Content-Type": "text/plain"});
-    // response.write("Hello World");
-    // response.end();
+    request.setEncoding("UTF-8");
 
+    request.addListener("data",function(postDataChunk){
+        postData += postDataChunk;
+        console.log("Receive POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end",function(){
+        route(handle,pathname,response,postData);
+    });
+
+}
 
 http.createServer(onRequest).listen(8888);
 console.log("Request has started");

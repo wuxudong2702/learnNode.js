8a41537634c192ee1943c5f74eececd3c53fb065
MagicConch <350043743@qq.com>
2017-02-25 15:46:55 +0800

    .

---
+++ demosInNodeJsInAction/code_5.3/link.js
@@ -0,0 +1,11 @@
+var mongodb = require('mongodb');
+var server = new mongodb.Server('127.0.0.1', 27017, {});
+var client = new mongodb.Db('mydatabase', server, { w:1 });
+
+client.open(function(err) {
+    if(err) throw err;
+    client.collection('test_insert', function(err, collection) {
+        if(err) throw err;
+        console.log('We are now able to perform queries');
+    });
+});

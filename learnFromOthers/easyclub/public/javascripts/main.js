697776618ac7751e20d6fe81b5adcee25b19b04c
MagicConch <350043743@qq.com>
2017-03-24 17:58:27 +0800

    ..

--- learnFromOthers/easyclub/public/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-
-  $(document).on('click.loading', '[data-loading]', function () {
-    var $this = $(this);
-    var method = $this.is('a, button') ? 'html' : 'val';
-    $this[method]($this.data('loading'));
-  });
-});
--- learnFromOthers/microblog/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	cookie_secret : 'secret_meteoric',
-	db : 'microblog',
-	host : 'localhost',
-	port : 27017
-}
